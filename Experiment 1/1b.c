30d41d75e2f3904129449fbc7a4d717bc4000739
Preet Singh <150048406+Preet2099x@users.noreply.github.com>
2024-08-07 06:51:31 +0530

    Update 1b.c

--- Experiment 1/1b.c
+++ Experiment 1/1b.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main(){
+    printf("Preet AJU/220501 \n");
     int n,i,sum=0;
     int avg = 0;
     printf("Enter no of elements: ");
     scanf("%d",&n);
 
     int arr[n];
-    printf("Enter the elements: ");
+    printf("Enter the elements: \n");
     for(i=0; i<n; i++){
         scanf("%d",&arr[i]);
         sum = sum + arr[i];
