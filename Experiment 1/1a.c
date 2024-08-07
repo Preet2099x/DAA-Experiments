cac7b8b2266d1f195e7d557dc48dcd6aa59345bb
Preet Singh <150048406+Preet2099x@users.noreply.github.com>
2024-08-07 06:49:53 +0530

    Update 1a.c

--- Experiment 1/1a.c
+++ Experiment 1/1a.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
+    printf("Preet AJU/220501 \n");
     int a[10] = {12,23,13,64,52,4,47,68,19,10};
     printf("The Elements of Array are: ");
     for(int i=0; i<10; i++)
