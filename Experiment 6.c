73c1c45e2a0d35327170c8e036eef198914f2417
Preet Singh <150048406+Preet2099x@users.noreply.github.com>
2024-08-07 07:39:10 +0530

    Update Experiment 6.c

--- Experiment 6.c
+++ Experiment 6.c
@@ -48,12 +48,13 @@ void mergeSort(int arr[], int low, int high) {
 }
 
 int main() {
+    printf("Preet AJU/220501 \n");
     int n;
     printf("Enter the size of the array: ");
     scanf("%d", &n);
 
     int arr[n];
-    printf("Enter the elements of the array: \n");
+    printf("Enter the elements of the array: ");
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
